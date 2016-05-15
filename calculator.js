c64832c0f7c29fa17fe3f80cc4ac02c7d6729ecf
Florent Revest <revestflo@gmail.com>
2016-05-15 14:14:56 +0200

    Fix license header

--- calculator.js
+++ calculator.js
@@ -3,9 +3,9 @@
  *               2011 - Nokia Corporation and/or its subsidiary(-ies).
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 2.1 of the
- * License, or (at your option) any later version.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
