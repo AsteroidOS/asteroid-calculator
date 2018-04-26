1880d08069cc0139c5521cc5b5c6cd610d2688c2
Nathan Follens <nathan@email.is>
2018-04-26 19:10:52 +0000

    Translated using Weblate (Flemish)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/calculator/nl_BE/

--- i18n/asteroid-calculator.nl_BE.ts
+++ i18n/asteroid-calculator.nl_BE.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-calculator.desktop.h" line="6"/>
         <source>Calculator</source>
-        <translation></translation>
+        <translation>Rekenmachien</translation>
     </message>
 </context>
 </TS>
