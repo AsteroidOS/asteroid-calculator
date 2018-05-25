16caeadc6cf33225d4df89bb5571569a3ee042a8
Mika Beckerich <beckerichmika@gmail.com>
2018-05-25 15:58:12 +0000

    Translated using Weblate (Luxembourgish)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/calculator/lb/

--- i18n/asteroid-calculator.lb.ts
+++ i18n/asteroid-calculator.lb.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-calculator.desktop.h" line="6"/>
         <source>Calculator</source>
-        <translation></translation>
+        <translation>Rechner</translation>
     </message>
 </context>
 </TS>
