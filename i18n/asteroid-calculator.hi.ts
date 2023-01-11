ea1c3457f9ef69903d88ea369f924466741ec5b4
Xyngg <heyabhinav@protonmail.com>
2023-01-11 19:46:25 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/calculator/hi/

--- i18n/asteroid-calculator.hi.ts
+++ i18n/asteroid-calculator.hi.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-calculator.desktop.h" line="6"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
+        <translation>कैलकुलेटर</translation>
     </message>
 </context>
 </TS>
