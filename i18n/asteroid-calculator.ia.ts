5d9e45e40e6347cd20270b13c72779b2180b9e02
Software In Interlingua <softinterlingua@gmail.com>
2024-01-24 18:46:03 +0000

    Translated using Weblate (Interlingua)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/calculator/ia/

--- i18n/asteroid-calculator.ia.ts
+++ i18n/asteroid-calculator.ia.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-calculator.desktop.h" line="6"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
+        <translation>Calculator</translation>
     </message>
 </context>
 </TS>
